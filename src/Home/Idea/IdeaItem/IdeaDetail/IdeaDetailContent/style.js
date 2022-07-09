d6fb01ca278b95a2455b32df73acf870fd0e53f3
joeygi <1478577769@qq.com>
2022-07-09 12:02:53 +0000

    src

---
+++ src/Home/Idea/IdeaItem/IdeaDetail/IdeaDetailContent/style.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components'
+export const ContentWrapper = styled.div`
+    border-top: 1px solid DarkGray;
+`
---
+++ src/Home/Idea/IdeaTop/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+export const IdeaTopWrapper = styled.div`
+    .swiper-container{/*把主要的框写好*/
+    border-top: 1px solid DarkGray;
+    width: 350px;
+    overflow: hidden;
+    /* margin-top:0.5rem; */
+    .swiper-slide{
+        text-align: center;
+        margin: 5px -15px;
+        width: 20px;
+        overflow-x:scroll;
+    }
+}
+
+`
