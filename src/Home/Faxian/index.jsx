88a6f405f1c08f4b588f3bc9757e9aee31a4bba4
joeygi <1478577769@qq.com>
2022-07-09 12:11:05 +0000

    删除文件 src/Home

--- src/Home/Faxian/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Recommend() {
-  return (
-    <div>发现</div>
-  )
-}
--- src/Home/Idea/IdeaItem/IdeaDetail/IdeaDetailContent/style.js
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components'
-export const ContentWrapper = styled.div`
-    border-top: 1px solid DarkGray;
-`
